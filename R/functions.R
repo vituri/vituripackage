1d8d4cb761fb2def2f877988c63dae440983bf75
Guilherme Vituri <56522687+vituri@users.noreply.github.com>
2021-05-26 11:38:28 -0300

    escreve_tabela_sql_geral

--- R/functions.R
+++ R/functions.R
@@ -801,4 +801,53 @@ converte_data_para_string = function(x, tipo = 'Semana') {
 
   return(y)
 }
-#
+
+#' Insere uma tabela no db via uma query gigante
+#' @param .data Dataframe para ser escrito no banco
+#' @param con Conexão com o database
+#' @param nome_tabela_db Nome da tabela no database
+#' @param comando Comando que vai ser usado na inserção
+#'
+#' @return O tanto de linhas que foi alterado (conta o insert e o update)
+#'
+#' @export
+#'
+escreve_tabela_sql_geral = function(.data, con, nome_tabela_db, comando = 'REPLACE') {
+
+  colunas_da_tabela = DBI::dbListFields(conn = con, name = nome_tabela_db)
+
+  colunas_em_comum = dplyr::intersect(colunas_da_tabela, names(.data))
+
+  colunas_excluidas = dplyr::setdiff(names(.data), colunas_da_tabela)
+
+  if (length(colunas_excluidas) != 0) {
+    warning(glue('Colunas {diferenca %>% paste(collapse = ', ')} foram descartadas...'))
+  }
+
+  temp =
+    .data %>%
+    select(any_of(colunas_em_comum)) %>%
+    tidyr::unite(col = '.unite', sep = "','") %>%
+    pull(.unite)
+
+  if (length(temp) == 0) {
+    warning('Nada para salvar')
+    return(NULL)
+  }
+
+  values =
+    glue("('{temp}')") %>%
+    glue_collapse(sep = ',')
+
+  nomes_colunas_formatado =
+    colunas_em_comum %>%
+    glue_collapse(sep = "`,`") %>%
+    paste0("(`", ., "`)")
+
+  query = glue("{comando} INTO `{nome_tabela_db}` {nomes_colunas_formatado} VALUES {values}")
+
+  # query
+
+  DBI::dbExecute(conn = con, statement = query)
+}
+

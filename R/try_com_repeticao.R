b6e1fe98c504c00f612613e5075f841f79859191
G. Vituri <56522687+vituri@users.noreply.github.com>
2023-07-13 11:55:41 -0300

    add try_com_repeticao

---
+++ R/try_com_repeticao.R
@@ -0,0 +1,36 @@
+#' Tenta executar um função repetidas vezes, com tempo de espera entre elas
+try_com_repeticao = function(
+    expr, tentativas = 5, texto_funcao = "função x", tempo_espera_entre_tentativas = 0
+    ,valor_se_erro = NULL
+) {
+  contador = 1
+
+  while (TRUE) {
+
+    # cli::cli_progress_step('Iteração {contador}...')
+    valor = try(expr = eval.parent(substitute(expr)), silent = TRUE)
+
+    # se não é erro, sai
+    if (argusinterno::is_try_error(valor) == FALSE) {
+
+      if (contador > 1) {
+        cli::cli_progress_step('Função {texto_funcao} executado na tentativa {contador}')
+      }
+
+      break
+    }
+
+    # se deu mais que tentativas, sai
+    if (contador >= tentativas) {
+      cli::cli_progress_step('Função {texto_funcao} tentou ser executada {contador} vezes, mas sem sucesso...')
+      valor = valor_se_erro
+      break
+    }
+
+    contador = contador + 1
+    Sys.sleep(tempo_espera_entre_tentativas)
+
+  }
+
+  return(valor)
+}
